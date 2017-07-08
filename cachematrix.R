a7285666a3d28ed856c88cc3c2ae5eb0c02be633
Cesar Villalobos <c.villalobos.luengo@gmail.com>
2017-07-08 01:10:14 -0400

    the function makeCacheMatrix and cacheSolve was created

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,62 @@
-## Put comments here that give an overall description of what your
-## functions do
+## First of all, you excuse my written English if it is not correct enough.
+## I'll try to explain my code as best I can
 
-
-## Write a short comment describing this function
+## The makeCacheMatrix  Function  has an input a Matrix (a square invertible matrix)
+## the result of makeCacheMatrix is the input of th e other function cacheSolve
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  #The result variable is the inverse matrix of result. I set to null
+  result <- NULL
+  
+  #Set a matrix to x variable
+  set <- function(y) {
+    x <<- y
+    result <<- NULL
+  }
+  
+  #get de data, in this case the matrix
+  get <- function() x
+  
+  #set de inverse martix
+  setInverse <- function(solve) result <<- solve
 
+  #get de inverse matrix
+  getInverse <- function() result
+  
+  # the final reurn is a list with the function set() get() setInverse() getInverse()
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve function return de Inverse Matrix, 
+##the second time to execute it will return a cache of the result 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  result <- x$getInverse()
+  
+  #In the case that result is not null, I return the result of Inverse Matrix 
+  if(!is.null(result)) {
+    message("getting cached data")
+    return(result)
+  }
+  
+  #In the case that the result is null
+  # It mean that is the first time, I do all the process to get the inverse matrix 
+  
+  
+  #get the data of the original Matrix.
+  matrixData <- x$get()
+  
+  #the result is the Inverse of the matrix
+  result <- solve(matrixData, ...)
+  
+  # I Set the Inverse of the matrix in x
+  x$setInverse(result)
+  
+  # Finally I return the result variable that is the inverse matrix
+  result
 }
